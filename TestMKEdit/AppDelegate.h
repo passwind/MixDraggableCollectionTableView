2263d955bb3273b1d4190adef7480ad532a4729e
Zhu Yu <mike.zhuyu@gmail.com>
2014-02-06 14:51:42 +0800

    Initial Commit

---
+++ TestMKEdit/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TestMKEdit
+//
+//  Created by Zhu Yu on 14-2-6.
+//  Copyright (c) 2014年 qcsoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TestMKEdit/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TestMKEdit
+//
+//  Created by Zhu Yu on 14-2-6.
+//  Copyright (c) 2014年 qcsoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
